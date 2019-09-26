61ed0e4720094e147fb341e38288df9b2613fe7b
Rasmus Erik Seremet Rosenkjær <zbcraros2@zbc.dk>
2019-09-26 12:08:15 +0200

    Commit

--- SparkGradleAPI/src/main/java/Main.java
+++ SparkGradleAPI/src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		get("/hello",(req,res) -> "Hello World");
 		get("/Piratos",(req,res) -> "Piratos fixede hele API'en helt alene ved hjælp af genstart");
 		get("/Changes",(req,res) -> "This is a test page");
+		get("/Changes2",(req,res) -> "This is a test page");
 	}
 	
 }
