2a07d1c5df05f7507f01f4777a7a4613d086ff54
Rasmus Erik Seremet Rosenkjær <zbcraros2@zbc.dk>
2019-09-26 13:45:56 +0200

    Update

--- SparkGradleAPI/src/main/java/Main.java
+++ SparkGradleAPI/src/main/java/Main.java
@@ -1,13 +1,40 @@
 import static spark.Spark.*;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+
+import spark.Request;
+import spark.Route;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		get("/hello",(req,res) -> "Hello World");
-		get("/Piratos",(req,res) -> "Piratos fixede hele API'en helt alene ved hjælp af genstart");
-		get("/Changes",(req,res) -> "This is a test page");
-		get("/Changes2",(req,res) -> "This is a test page");
+		//This is test
+		//get("/MM",(req,res) -> doStuff(req));
+		new TestController();
+		
+		//post("/MM", (req,res) -> doStuff(req));
+		
 	}
 	
+
+	public static String doStuff(Request req) 
+	{
+		
+		
+		Set<String> m =req.queryParams();
+		Iterator i = m.iterator();
+		while(i.hasNext())
+				
+		{
+			System.out.println(req.queryParams(i.next().toString()));
+			
+		}
+		
+		return "lalala";
+	}
+
 }
