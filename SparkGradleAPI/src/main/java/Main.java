3adaee9c70499dd0726c2dffd8a6b54c8ebd1f9a
Rasmus Erik Seremet Rosenkjær <zbcraros2@zbc.dk>
2019-09-26 12:02:01 +0200

    Added Gradle, and made 2 pages.

---
+++ SparkGradleAPI/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import static spark.Spark.*;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		get("/hello",(req,res) -> "Hello World");
+		get("/Piratos",(req,res) -> "Piratos fixede hele API'en helt alene ved hjælp af genstart");
+	}
+	
+}
